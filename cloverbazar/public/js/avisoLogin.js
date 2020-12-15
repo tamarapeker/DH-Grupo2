961716ee474da04d808dd9b32527d248d4731d7e
Martin Jaime Levy <martinjaimelevy@gmail.com>
2020-12-14 21:01:18 -0300

    clase de a tres

--- cloverbazar/public/js/avisoLogin.js
+++ cloverbazar/public/js/avisoLogin.js
@@ -1,33 +1,32 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     console.log("estoy funcionando")
-        // Get the modal
-        var modal = document.getElementById("myModal-login");
+    // Get the modal
+    var modal = document.getElementById("myModal-login");
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn-login");
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn-login");
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close-login")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close-login")[0];
 
-        // When the user clicks on the button, open the modal
-        btn.onclick = function() {
-            modal.style.display = "block";
-            console.log("clickeando")
-        }
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+        console.log("clickeando")
+    }
 
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            console.log("cuando click en cruz")
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        console.log("cuando click en cruz")
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
             modal.style.display = "none";
         }
+    }
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            console.log("Funcion de click afuers")
-            if (event.target == modal) {
-                console.log("cuando click afuera")
-                modal.style.display = "none";
-            }
-        }
-    
+
 })
