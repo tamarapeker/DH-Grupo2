2a833250661d1f7d893d5abc6a592036ac61f6d9
tamarapeker <tamipeker@gmail.com>
2020-12-17 12:16:43 -0300

    formularios por mail e imagenes

--- cloverbazar/controllers/mainController.js
+++ cloverbazar/controllers/mainController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const db = require('../database/models');
-
+const nodemailer = require("nodemailer");
 
 
 const mainController = {
@@ -101,6 +101,82 @@ const mainController = {
 
         }
         res.render('mayorista');
+    },
+    mandarMail: function(req,res,next){
+        // async..await is not allowed in global scope, must use a wrapper
+        async function main() {
+            // Generate test SMTP service account from ethereal.email
+            // Only needed if you don't have a real mail account for testing
+            let testAccount = await nodemailer.createTestAccount();
+
+            // create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                user: "ventascloverbazar@gmail.com", // generated ethereal user
+                pass: "alantami2020", // generated ethereal password
+                },
+            });
+
+            // send mail with defined transport object
+            let info = await transporter.sendMail({
+                from: req.body.email, // sender address
+                to: "ventascloverbazar@gmail.com", // list of receivers
+                subject: req.body.asunto, // Subject line
+                text: req.body.mensaje, // plain text body
+                html: "", // html body
+            });
+
+            console.log("Message sent: %s", info.messageId);
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+            // Preview only available when sending through an Ethereal account
+            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        }
+
+        main().catch(console.error);
+        res.redirect('/')
+    },
+    mandarMailMayorista: function(req,res,next){
+        // async..await is not allowed in global scope, must use a wrapper
+        async function main() {
+            // Generate test SMTP service account from ethereal.email
+            // Only needed if you don't have a real mail account for testing
+            let testAccount = await nodemailer.createTestAccount();
+
+            // create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                user: "ventascloverbazar@gmail.com", // generated ethereal user
+                pass: "alantami2020", // generated ethereal password
+                },
+            });
+
+            // send mail with defined transport object
+            let info = await transporter.sendMail({
+                from: req.body.email, // sender address
+                to: "ventascloverbazar@gmail.com", // list of receivers
+                subject: "Mayorista: "+req.body.nombre, // Subject line
+                text: "Email: "+req.body.email+"\n"+"Celular: "+req.body.celular + "\n" +req.body.mensaje, // plain text body
+                html: "", // html body
+            });
+
+            console.log("Message sent: %s", info.messageId);
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+            // Preview only available when sending through an Ethereal account
+            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        }
+
+        main().catch(console.error);
+        res.redirect('/')
     }
 }
 
